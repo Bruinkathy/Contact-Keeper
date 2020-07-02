48fcfecace4ee9dedfa22c16d7ee71fe9a33953e
Kathy Bartholomew <bruinkathy@gmail.com>
2020-07-01 22:05:20 -0700

    Backend created Users, Contacts, and Authentication

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.json({msg:'Hello World'}));
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Hello World' }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
